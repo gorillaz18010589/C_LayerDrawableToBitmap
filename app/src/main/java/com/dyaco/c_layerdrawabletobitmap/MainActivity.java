45da0a41174c2ba1772289c9223dde68f60d8f63
gorillaz18010589 <gorillaz1801058@gmail.com>
2021-10-04 15:58:27 +0800

    v2新增getImageBytesLevelOverIncline,getImageBytesInclineOverLevel,incline覆蓋level圖片 (1).bar level > incline   l紅/in綠 (2).bar level = incline   綠色覆蓋 (3).br  level < incline   l綠/in青綠色

--- app/src/main/java/com/dyaco/c_layerdrawabletobitmap/MainActivity.java
+++ app/src/main/java/com/dyaco/c_layerdrawabletobitmap/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 //    private String inclineNum = "5#5#5#5#5#5#5#10#10#10#10#10#10#10#10#10#10#10#10#10";
 
     private String levelNum   = "7#7#7#8#8#8#10#9#9#9#10#9#9#9#9#8#8#7#7#7";
-    private String inclineNum = "7#7#1#1#2#2#3#3#3#3#3#2#2#2#1#1#1#1#1#1";
+    private String inclineNum = "7#7#1#1#10#2#3#3#3#3#3#2#2#2#1#1#1#1#1#1";
     //    private String data2 = "1#2#10#10#1#5#5#5#5#3#1#3#4#4#3#6#3#3#6#4";
     private ImageView image;
     private ImageView image2;
