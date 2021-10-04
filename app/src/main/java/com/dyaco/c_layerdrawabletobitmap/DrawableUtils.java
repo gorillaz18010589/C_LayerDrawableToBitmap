259bb07de2210028e8322674d18e56a8516913b0
gorillaz18010589 <gorillaz1801058@gmail.com>
2021-10-04 15:51:53 +0800

    v2新增getImageBytesLevelOverIncline,getImageBytesInclineOverLevel,incline覆蓋level圖片 (1).bar level > incline   l紅/in綠 (2).bar level = incline   綠色覆蓋 (3).br  level < incline   l綠/in青綠色

--- app/src/main/java/com/dyaco/c_layerdrawabletobitmap/DrawableUtils.java
+++ app/src/main/java/com/dyaco/c_layerdrawabletobitmap/DrawableUtils.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 import android.graphics.drawable.PaintDrawable;
 import android.os.Build;
+import android.util.Log;
 import android.view.Gravity;
 
 import androidx.annotation.ColorRes;
@@ -54,4 +55,152 @@ public class DrawableUtils {
     }
 
 
+    /**
+     * 取得level的點線圖,搭配getImageBytesLevelOverIncline相互使用,且用另一個inclineImageView覆蓋在此imgView上使用
+     *
+     * @param context
+     * @param levelNum
+     * @param inclineNum
+     * @param height
+     * @param color      //level大於等於incline的預設顏色
+     * @return
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static byte[] getImageBytesInclineOverLevel(Context context, String levelNum, String inclineNum, int height, @ColorRes int color) {
+        int defaultLayoutHeight = 80;
+        int defaultLayoutWidth = 65;
+        int levelColor = R.color.green;  //當level小於incline, level的值改成incline顯示,並且顯示為綠色
+
+        int[] arrayLevel = Arrays.stream(levelNum.split("#", -1))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int[] arrayIncline = Arrays.stream(inclineNum.split("#", -1))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        final List<Drawable> images = new ArrayList<>();
+
+
+        for (int i = 0; i < arrayLevel.length; i++) {
+            PaintDrawable drawable = new PaintDrawable();
+
+
+            if (arrayLevel[i] > arrayIncline[i]) {
+                drawable.getPaint().setColor(context.getColor(color));
+                Log.v("hank", "getImageBytesInclineOverLevel 大於 -> i:" + i);
+
+            } else if (arrayLevel[i] == arrayIncline[i]) {
+                drawable.getPaint().setColor(context.getColor(color));
+                Log.v("hank", "getImageBytesInclineOverLevel 等於 -> i:" + i);
+
+
+                //當level小於incline時綠色顯示為level,故level,incline互相交換值
+            } else if (arrayLevel[i] < arrayIncline[i]) {
+                drawable.getPaint().setColor(context.getResources().getColor(levelColor));
+                int level = arrayLevel[i];
+                int incline = arrayIncline[i];
+
+                arrayIncline[i] = level;
+                arrayLevel[i] = incline;
+                Log.v("hank", "getImageBytesInclineOverLevel 小於 -> i:" + i + "arrayLevel[i] :" + arrayLevel[i] + ", arrayIncline[i]:" + arrayIncline[i]);
+
+            }
+
+
+            images.add(drawable);
+        }
+
+
+        final LayerDrawable layerDrawable = new LayerDrawable(images.toArray(new Drawable[0]));
+
+        int n = 0;
+        for (int i = 0; i < 20; i++) {
+            layerDrawable.setLayerGravity(i, Gravity.START | Gravity.BOTTOM);
+            layerDrawable.setLayerSize(i, defaultLayoutWidth, defaultLayoutHeight * arrayIncline[i]);
+            layerDrawable.setLayerInsetLeft(i, n);
+            n += 80;
+        }
+        Bitmap bitmap = Bitmap.createBitmap(layerDrawable.getIntrinsicWidth(), height, Bitmap.Config.ARGB_8888);
+        layerDrawable.setBounds(0, 0, layerDrawable.getIntrinsicWidth(), height);
+        layerDrawable.draw(new Canvas(bitmap));
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 10, stream);
+
+        return stream.toByteArray();
+    }
+
+
+    /**
+     * 取得Incline的點線圖,搭配getImageBytesInclineOverLevel相互使用,此方法是覆蓋在levelImageView上去做使用
+     *
+     * @param context
+     * @param levelNum
+     * @param inclineNum
+     * @param height
+     * @param color   //level大於等於incline的預設顏色
+     * @return
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static byte[] getImageBytesLevelOverIncline(Context context, String levelNum, String inclineNum, int height, @ColorRes int color) {
+        int defaultLayoutHeight = 80;
+        int defaultLayoutWidth = 65;
+        int inclineColor = R.color.green_light;  //當level小於incline, level的值改成incline顯示,並且顯示為青綠色
+
+        int[] arrayLevel = Arrays.stream(levelNum.split("#", -1))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int[] arrayIncline = Arrays.stream(inclineNum.split("#", -1))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        final List<Drawable> images = new ArrayList<>();
+
+
+        for (int i = 0; i < arrayLevel.length; i++) {
+            PaintDrawable drawable = new PaintDrawable();
+
+
+            if (arrayLevel[i] > arrayIncline[i]) {
+                drawable.getPaint().setColor(context.getColor(color));
+                Log.v("hank", "getImageBytesLevelOverIncline 大於 -> i:" + i);
+
+            } else if (arrayLevel[i] == arrayIncline[i]) {
+                drawable.getPaint().setColor(context.getColor(color));
+                Log.v("hank", "getImageBytesLevelOverIncline 等於 -> i:" + i);
+
+                //當level小於incline時青綠色顯示為incline,綠色顯示為level,level,incline互相交換值
+            } else if (arrayLevel[i] < arrayIncline[i]) {
+                drawable.getPaint().setColor(context.getResources().getColor(inclineColor));
+                int level = arrayLevel[i];
+                int incline = arrayIncline[i];
+
+                arrayIncline[i] = level;
+                arrayLevel[i] = incline;
+                Log.v("hank", "getImageBytesLevelOverIncline 小於 -> i:" + i + "arrayLevel[i] :" + arrayLevel[i] + ", arrayIncline[i]:" + arrayIncline[i]);
+
+            }
+
+            images.add(drawable);
+        }
+
+
+        final LayerDrawable layerDrawable = new LayerDrawable(images.toArray(new Drawable[0]));
+
+        int n = 0;
+        for (int i = 0; i < 20; i++) {
+            layerDrawable.setLayerGravity(i, Gravity.START | Gravity.BOTTOM);
+            layerDrawable.setLayerSize(i, defaultLayoutWidth, defaultLayoutHeight * arrayLevel[i]);
+            layerDrawable.setLayerInsetLeft(i, n);
+            n += 80;
+        }
+        Bitmap bitmap = Bitmap.createBitmap(layerDrawable.getIntrinsicWidth(), height, Bitmap.Config.ARGB_8888);
+        layerDrawable.setBounds(0, 0, layerDrawable.getIntrinsicWidth(), height);
+        layerDrawable.draw(new Canvas(bitmap));
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 10, stream);
+
+        return stream.toByteArray();
+    }
 }
--- app/src/main/java/com/dyaco/c_layerdrawabletobitmap/MainActivity.java
+++ app/src/main/java/com/dyaco/c_layerdrawabletobitmap/MainActivity.java
@@ -12,13 +12,12 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 public class MainActivity extends AppCompatActivity {
-    private String data  = "1#10#10#10#10#10#10#10#10#6#6#6#6#6#6#6#6#6#6#6";
-    private String data2 = "1#2#10#10#1#5#5#5#5#3#1#3#4#4#3#6#3#3#6#4";
+//    private String levelNum = "1#4#10#3#10#10#10#2#10#6#6#6#1#1#1#6#6#6#6#6";
+//    private String inclineNum = "5#5#5#5#5#5#5#10#10#10#10#10#10#10#10#10#10#10#10#10";
 
-    //    private String data  = "1#10#10#10#10#10#10#10#10#6#6#6#6#6#6#6#6#6#6#6";
-
-//    private String data2 = "3#1#5#5#5#5#5#5#5#3#6#3#4#4#3#6#3#3#6#4";
-
+    private String levelNum   = "7#7#7#8#8#8#10#9#9#9#10#9#9#9#9#8#8#7#7#7";
+    private String inclineNum = "7#7#1#1#2#2#3#3#3#3#3#2#2#2#1#1#1#1#1#1";
+    //    private String data2 = "1#2#10#10#1#5#5#5#5#3#1#3#4#4#3#6#3#3#6#4";
     private ImageView image;
     private ImageView image2;
 
@@ -34,8 +33,11 @@ public class MainActivity extends AppCompatActivity {
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     private void setImageView() {
-        byte[] imageBytes = DrawableUtils.getImageBytes(this, data, 800, R.color.red);
-        byte[] imageBytes2 = DrawableUtils.getImageBytes(this, data2, 800, R.color.green);
+        //byte[] imageBytes = DrawableUtils.getImageBytes(this, data, 800, R.color.red, true);
+        //byte[] imageBytes2 = DrawableUtils.getImageBytes(this, data2, 800, R.color.green);
+
+        byte[] imageBytes = DrawableUtils.getImageBytesLevelOverIncline(this, levelNum, inclineNum, 800, R.color.red);
+        byte[] imageBytes2 = DrawableUtils.getImageBytesInclineOverLevel(this, levelNum, inclineNum, 800, R.color.green);
 
         Glide.with(this)
                 .load(imageBytes)
